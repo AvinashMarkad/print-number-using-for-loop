75032a8becf611f1d126d69fe1753f153fcef276
Aviansh Ganpat Markad <100151747+AvinashMarkad@users.noreply.github.com>
2022-03-09 20:05:00 +0530

    Add files via upload

---
+++ Printnum.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Printnum
+{
+public static void main(String arg[])
+{
+int a,i;
+Scanner sc=new Scanner(System.in);
+
+System.out.println("Enter number you have print number:");
+i=sc.nextInt();
+
+for(a=0;a<=i;a++)
+{
+System.out.println(a);
+}
+}
+}
